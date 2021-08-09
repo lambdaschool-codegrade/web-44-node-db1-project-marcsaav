e9a4e88a3b316c2f4ca913a3ca958806dda2703f
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-09 14:33:01 -0700

    Accounts-Model implemented

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,25 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  return db('accounts').insert(account).then((ids) => {getById(ids[0])})
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).update(account).then(() => {getById(id)})
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  const accountToDel = getById(id)
+  db('accounts').where('id', id).delete()
+  return accountToDel
 }
 
 module.exports = {
